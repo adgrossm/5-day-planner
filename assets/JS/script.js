cb715934bc98e6aaa6d728ab2bce68475858a178
Adam Grossman <adgrossm@gmail.com>
2020-08-11 20:06:37 -0400

    set up HTML, CSS, JS and started layout for the rows

---
+++ assets/JS/script.js
@@ -0,0 +1,16 @@
+
+//----define moment varibles used to show time----//
+var date = moment().format("MMM Do YYYY");
+// var currentTime = moment().format('H');
+// console.log(currentTime);
+
+//-------append date----------//
+$("#currentDay").append(date);
+
+
+//---------background Updater------------------------//
+
+var timeOfDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+
+
+
