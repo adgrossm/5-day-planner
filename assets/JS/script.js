a5a440167de59720406d7a5e8f9ef1cf26c7c41b
Adam Grossman <adgrossm@gmail.com>
2020-08-11 20:33:06 -0400

    adding JS logic to change background color and to save to localStorage

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,5 +12,62 @@ $("#currentDay").append(date);
 
 var timeOfDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 
+udpateTime();
 
+function udpateTime() {
+  var currentTime = moment().format('H');
+  console.log(currentTime)
+  for(var i = 0; i < timeOfDay.length; i++) {
 
+    if (parseInt(timeOfDay[i]) > currentTime) {
+      $("#" + timeOfDay[i]).attr("style", "background-color: #77dd77");
+        // $("#" + timeOfDay[i]).addClass(".future");
+
+    }
+    else if (parseInt(timeOfDay[i]) < currentTime) {
+      $("#" + timeOfDay[i]).attr("style", "background-color: #d3d3d3");
+
+    }
+    else if (parseInt(timeOfDay[i]) == currentTime) {
+      $("#" + timeOfDay[i]).attr("style", "background-color: #ff6961");
+    
+    }
+  }
+}
+
+//-------Getting individual childern and setting to parent-----//
+$("#9am").children("input").val(localStorage.getItem("9am"));
+
+$("#10am").children("input").val(localStorage.getItem("10am"));
+
+$("#11am").children("input").val(localStorage.getItem("11am"));
+
+$("#12pm").children("input").val(localStorage.getItem("12pm"));
+
+$("#1pm").children("input").val(localStorage.getItem("1pm"));
+
+$("#2pm").children("input").val(localStorage.getItem("2pm"));
+
+$("#3pm").children("input").val(localStorage.getItem("3pm"));
+
+$("#4pm").children("input").val(localStorage.getItem("4pm"));
+
+$("#5pm").children("input").val(localStorage.getItem("5pm"));
+
+//--onclick event to save user input to local storage---//
+$(".saveBtn").on("click", function() {
+  var timeOfday = $(this).parent().attr("id");
+  var textContent = $("input").val().trim();
+
+  localStorage.setItem(timeOfday, textContent);
+  console.log(timeOfday, textContent);
+});
+// $(".saveBtn").on("click", function() {
+ 
+//     var timeOfDay = $(this).parent().attr("id");
+//     // var textContent = $(this).input().val().trim();
+//     var textContent = $("input").val().trim();
+
+//     localStorage.setItem(timeOfDay, textContent);
+//     console.log(timeOfDay, textContent);
+// });
